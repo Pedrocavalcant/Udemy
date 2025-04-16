fcaf5015cbc588429c717d1d2875242b067f44a1
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-04-16 11:17:06 -0300

    includes, endsWith e startsWith

---
+++ att18/index18.js
@@ -0,0 +1,20 @@
+//let nomes = ['Pedro', 'Lucas', 'Mateus']
+
+/*nomes.includes('Lucas')  //busca de um determinado valor dentro da variavel
+
+console.log(nomes.includes('Lucas'))
+
+if(nomes.includes('Pedro')) {
+    console.log('Pedro esta na lista')
+} else{
+    console.log('Pedro nao esta na lista')
+}*/
+
+/*let nome = 'Pedro' //verifica se termina com uma letra/frase
+
+console.log(nome.endsWith('o'))*/
+
+let nome = 'Pedro'
+
+console.log(nome.startsWith('Ped')) //verifica se comeca com uma letra/frase
+
