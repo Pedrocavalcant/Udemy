d493896cf87567e6b58d914994e5ccd4231fcf1d
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-04-17 01:30:01 -0300

    implementacao de componentes

--- react/att01/src/App.js
+++ react/att01/src/App.js
@@ -1,8 +1,23 @@
-export default function App() {
+import react from 'react'
+
+const BemVindo = (props) => {
   return(
     <div>
-      <h1>Bem vindo ao sistema</h1>
-      <h2>@Sujeitoprogramador :)</h2>
+      <h2>Bem-vindo(a) {props.nome}</h2>
+      <h3>Tenho {props.idade} Anos</h3>
     </div>
   )
 }
+
+function App() {
+  return(
+    <div>
+      Ola mundo!
+      <BemVindo nome="Pedro" idade = "20"/>
+      <BemVindo nome="Mateus" idade = "17"/>
+      <h1>Curso React</h1>
+    </div>
+  )
+}
+
+export default App
