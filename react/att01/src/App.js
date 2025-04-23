c1d3634c0ec2d938215567ae70f994d375ef8081
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-04-23 17:29:54 -0300

    Ciclo de vida no react

--- react/att01/src/App.js
+++ react/att01/src/App.js
@@ -1,45 +1,37 @@
-import react from 'react'
+import React, { Component } from 'react'
 
-const Equipe = (props) => {
-  return (
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
-      <Social github={props.github} linkedin={props.linkedin}/>
-    </div>
-  )
-}
+class App extends Component{
 
-const Sobre = (props) => {
-  return(
-    <div>
-      <h2>Ola sou o(a) {props.nome}</h2>
-      <h3>Cargo: {props.cargo}</h3>
-      <h3>Idade: {props.idade} anos</h3>
-    </div>
-  )
-}
+  constructor(props){
+    super(props)
+    this.state = {
+      hora: '00:00:00'
+     }
+  }
 
-const Social = (props) => {
-  return(
-    <div>
-      <a href={props.github}>Github </a>
-      <a href={props.linkedin}>Linkedin </a>
-      <a>Youtube </a>
-    </div>
-  )
-}
+  componentDidMount(){
 
+    setInterval(() =>{
+      this.setState({hora: new Date().toLocaleTimeString()})
+    }, 1000)
 
-function App() {
-  return(
-    <div>
-      <h1>Conheca nossa equipe</h1>
-      <Equipe nome="Pedro" cargo="Web Developer" idade="20" github="https://github.com/Pedrocavalcant" linkedin="https://www.linkedin.com/in/pedro-lucas-cavalcante/"/>  <hr/>
-      <Equipe nome="Mateus" cargo="Video Maker" idade="17" />  <hr/>
-      <Equipe nome="Lucas" cargo="Programador" idade="19" />
+  }
 
-    </div>
-  )
+  componentDidUpdate(){
+    console.log('Atualizou!!!')
+  }
+
+  shouldComponentUpdate(){
+    
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Meu projeto {this.state.hora}</h1>
+      </div>
+    )
+  }
 }
 
 export default App
