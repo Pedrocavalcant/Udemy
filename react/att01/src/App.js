4ef2fafa032ea75453e49bb4cfd2adf77a8eb73e
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-04-23 00:40:18 -0300

    Adicionando funcoes no react

--- react/att01/src/App.js
+++ react/att01/src/App.js
@@ -1,21 +1,43 @@
 import react from 'react'
 
-const BemVindo = (props) => {
-  return(
+const Equipe = (props) => {
+  return (
     <div>
-      <h2>Bem-vindo(a) {props.nome}</h2>
-      <h3>Tenho {props.idade} Anos</h3>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+      <Social github={props.github} linkedin={props.linkedin}/>
     </div>
   )
 }
 
+const Sobre = (props) => {
+  return(
+    <div>
+      <h2>Ola sou o(a) {props.nome}</h2>
+      <h3>Cargo: {props.cargo}</h3>
+      <h3>Idade: {props.idade} anos</h3>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return(
+    <div>
+      <a href={props.github}>Github </a>
+      <a href={props.linkedin}>Linkedin </a>
+      <a>Youtube </a>
+    </div>
+  )
+}
+
+
 function App() {
   return(
     <div>
-      Ola mundo!
-      <BemVindo nome="Pedro" idade = "20"/>
-      <BemVindo nome="Mateus" idade = "17"/>
-      <h1>Curso React</h1>
+      <h1>Conheca nossa equipe</h1>
+      <Equipe nome="Pedro" cargo="Web Developer" idade="20" github="https://github.com/Pedrocavalcant" linkedin="https://www.linkedin.com/in/pedro-lucas-cavalcante/"/>  <hr/>
+      <Equipe nome="Mateus" cargo="Video Maker" idade="17" />  <hr/>
+      <Equipe nome="Lucas" cargo="Programador" idade="19" />
+
     </div>
   )
 }
