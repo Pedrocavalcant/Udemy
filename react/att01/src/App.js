9d5cb5f03c5e68805fa4a663027b08ebf508acb5
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-05-06 15:26:55 -0300

    Commit segunda atividade de react

--- react/att01/src/App.js
+++ react/att01/src/App.js
@@ -1,37 +1,11 @@
-import React, { Component } from 'react'
-
-class App extends Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-      hora: '00:00:00'
-     }
-  }
-
-  componentDidMount(){
-
-    setInterval(() =>{
-      this.setState({hora: new Date().toLocaleTimeString()})
-    }, 1000)
-
-  }
-
-  componentDidUpdate(){
-    console.log('Atualizou!!!')
-  }
-
-  shouldComponentUpdate(){
-    
-  }
-
-  render(){
-    return(
-      <div>
-        <h1>Meu projeto {this.state.hora}</h1>
-      </div>
-    )
-  }
+export default function App(){
+  return(
+    <div>
+      <h1 id='teste'>Bem vindo ao sistema</h1>
+      <h2>@Sujeitoprogramador :)</h2>
+      <h3>praticando react</h3>
+      <h4>&copyPedrocavalcante</h4>
+    </div>
+  )
 }
 
-export default App
---
+++ react/att02/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const BemVindo = (props) => {
+  return(
+    <div>
+      <h2>Meu nome é: {props.nome}</h2>
+      <h3>Minha idade é: {props.idade}</h3>
+      <h3>Sou {props.genero}</h3>
+      <h3>Trabalho com: {props.emprego}</h3>
+    </div>
+  )
+}
+
+function App(){
+  return(
+    <div>
+      Olá, Mundo!
+     <BemVindo nome= "Pedro" idade = "20" genero = "Homem" emprego = "Desenvolvedor"/>
+    </div>
+  )
+}
+
+export default App;
