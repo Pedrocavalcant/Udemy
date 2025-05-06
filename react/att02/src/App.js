4a8ba2768b20ee0fd55c5aeca3caf74a3dff7c1a
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-05-06 15:31:38 -0300

    alteração de function

--- react/att02/src/App.js
+++ react/att02/src/App.js
@@ -6,7 +6,7 @@ const BemVindo = (props) => {
       <h2>Meu nome é: {props.nome}</h2>
       <h3>Minha idade é: {props.idade}</h3>
       <h3>Sou {props.genero}</h3>
-      <h3>Trabalho com: {props.emprego}</h3>
+      <h3>Trabalho com: {props.emprego}</h3><br></br><hr></hr>
     </div>
   )
 }
@@ -15,7 +15,9 @@ function App(){
   return(
     <div>
       Olá, Mundo!
-     <BemVindo nome= "Pedro" idade = "20" genero = "Homem" emprego = "Desenvolvedor"/>
+     <BemVindo nome= "Pedro" idade = "20" genero = "Homem" emprego = "Desenvolvedor"/> 
+     <BemVindo nome = "Mateus" idade = "17" genero = "Homem" emprego = "Gestor de tráfego"/>
+     <BemVindo nome = "Lucas" idade = "20" genero = "Homem" emprego = "Técnico de Sistema"/>
     </div>
   )
 }
