165432a9d6aebea1ccb8df496a294607bc35841c
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-05-12 21:13:07 -0300

    sexta atividade de react

--- react/att06/src/App.js
+++ react/att06/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hora: '00:00:00'
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString() });
+    }, 1000)
+  } //quando vai consumir uma API extra, vai pegar um webservice
+
+  componentDidUpdate() {
+    console.log('Atualizou!')
+  } //se teve alguma atualização fazer alguma ação
+
+  /*shouldComponentUpdate(){
+  retorna um true ou false
+  }*/ 
+
+  render() {
+    return (
+      <div>
+        <h1>Meu projeto {this.state.hora}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
